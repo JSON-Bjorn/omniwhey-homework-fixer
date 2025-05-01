87651f741ca4b5f88111a1faa260c1a48af9ee60
Björn Revell <mmabjorn@hotmail.com>
2025-05-01 11:00:12 +0200

    New file structure

---
+++ backend/models/models.py
@@ -0,0 +1,7 @@
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+
+Base = declarative_base()
+
+# Add your models here
